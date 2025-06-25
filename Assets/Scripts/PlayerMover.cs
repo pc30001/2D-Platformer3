bf10f4dc2ed21071ad1e4864ba700845a78ecb14
pc30001 <itcomputer@mail.ru>
2025-06-25 16:05:42 +0300

    jump

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -7,16 +7,13 @@ public class PlayerMover : MonoBehaviour
 {
     private const float SPEED_COEFFICIENT = 50;
     private const string HORIZONTAL_AXIS = "Horizontal";
-    private const string VERTICAL_AXIS = "Vertical";
     private const string GROUND_TAG = "Ground";
 
     [SerializeField] private float _speedX = 1;
-    [SerializeField] private float _speedY = 1;
     [SerializeField] private float _jumpForce = 500;
 
     private Rigidbody2D _rigibody;
     private float _direction;
-    private float _verh;
     private bool _isJump;
     private bool _isGround;
 
@@ -29,36 +26,29 @@ public class PlayerMover : MonoBehaviour
     {
 
         _direction = Input.GetAxis(HORIZONTAL_AXIS);
-        _verh = Input.GetAxis(VERTICAL_AXIS);
 
-        //if (_isGround && Input.GetKeyDown(KeyCode.W))
-        //{
-        //    _isJump = true;
-        //}
+        if (_isGround && Input.GetKeyDown(KeyCode.W))
+        {
+            _isJump = true;
+        }
     }
 
     private void FixedUpdate()
     {
 
-        //_rigibody.velocity = new Vector2(_speedX * _direction * SPEED_COEFFICIENT * Time.fixedDeltaTime, _rigibody.velocity.y);
-        //_rigibody.velocity = new Vector2(_speedY * _verh * SPEED_COEFFICIENT * Time.fixedDeltaTime, _rigibody.velocity.x);
+        _rigibody.velocity = new Vector2(_speedX * _direction * SPEED_COEFFICIENT * Time.fixedDeltaTime, _rigibody.velocity.y);
 
-        float moveX = _speedX * _direction * SPEED_COEFFICIENT * Time.fixedDeltaTime;
-        float moveY = _speedY * _verh * SPEED_COEFFICIENT * Time.fixedDeltaTime;
-        
-        _rigibody.velocity = new Vector2(moveX, moveY);
-
-        //if (_isJump)
-        //{
-        //    _rigibody.AddForce(new Vector2(0, _jumpForce));
-        //    _isJump = false;
-        //    _isGround = false;
-        //}
+        if (_isJump)
+        {
+            _rigibody.AddForce(new Vector2(0, _jumpForce));
+            _isJump = false;
+            _isGround = false;
+        }
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag(GROUND_TAG))
-    //        _isGround = true;
-    //}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+            _isGround = true;
+    }
 }
