8aec539eac110d56a143b5903b1bb044521f990a
pc30001 <itcomputer@mail.ru>
2025-07-22 16:36:48 +0300

    добавил врага

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private WayPoint[] _wayPoints;
+    [SerializeField] private float _speedX = 1;
+
+    private Rigidbody2D _rigibody;
+    private bool _isTurnRight = true;
+    private int _wayPointIndex;
+    private Transform _target;
+    [SerializeField] private float _maxSqrDistance = 0.1f;
+
+    private void Start()
+    {
+        _rigibody = GetComponent<Rigidbody2D>();
+        _target = _wayPoints[_wayPointIndex].transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (IsTargetReached())
+            ChangeTarget();
+    }
+
+    private void Move()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(transform.position, _target.position, _speedX * Time.fixedDeltaTime);
+        _rigibody.MovePosition(newPosition);
+    }
+
+    private bool IsTargetReached()
+    {
+        float sqrDistance = (transform.position - _target.position).sqrMagnitude;
+        return sqrDistance < _maxSqrDistance;
+    }
+
+    private void ChangeTarget()
+    {
+        //_wayPointIndex++;
+        //if (_wayPointIndex >= _wayPoints.Length)
+        //    _wayPointIndex = 0;
+
+        _wayPointIndex = ++_wayPointIndex % _wayPoints.Length;
+        _target = _wayPoints[_wayPointIndex].transform;
+
+        if ((transform.position.x < _target.position.x && _isTurnRight == false)
+         || (transform.position.x > _target.position.x && _isTurnRight))
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        _isTurnRight = !_isTurnRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
